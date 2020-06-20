31c112e2b39d20c66b22e8a845f86ca14b3b982b
simonojok19 <simonojok19@gmail.com>
2020-06-20 16:52:22 +0300

    all severs are running

--- src/server.js
+++ src/server.js
@@ -6,21 +6,71 @@ import { connect } from './db'
 import product from './types/product/product.resolvers'
 import coupon from './types/coupon/coupon.resolvers'
 import user from './types/user/user.resolvers'
+import { Query } from 'mongoose'
 
 const types = ['product', 'coupon', 'user']
 
 export const start = async () => {
   const rootSchema = `
+    type Cat {
+      name: String
+      age: Int!
+      ower: Owner
+    }
+
+    type Owner {
+      name: String
+      cat: Cat
+    }
+
+    type Query {
+      cat(name: String!): Cat!
+      owner(name: String!): Owner!
+    }
     schema {
       query: Query
-      mutation: Mutation
     }
   `
   const schemaTypes = await Promise.all(types.map(loadTypeSchema))
 
   const server = new ApolloServer({
-    typeDefs: [rootSchema, ...schemaTypes],
-    resolvers: merge({}, product, coupon, user),
+    typeDefs: [rootSchema],
+    resolvers: {
+      Query: {
+        cat(first, args, context, info) {
+          console.log('In Query.cat()')
+          return {}
+        },
+        owner(first, args, context, info) {
+          console.log('In Query.owner()')
+          return {}
+        },
+        Cat: {
+          name(previous, args, constext, info) {
+            console.log('In Query.Cat.name()')
+            return 'Donie'
+          },
+          age(previous, args, context, info) {
+            console.log('In Query.Cat.age()')
+            return 2
+          },
+          owner(previous, args, constex, info) {
+            console.log('In Query.Cat.owner()')
+            return {}
+          }
+        },
+        Owner: {
+          name(previous, args, context, info) {
+            console.log('In Query.Owner.name()')
+            return 'Simon Ojok'
+          },
+          cat(previous, args, context, infor) {
+            console.log('In Query.Owner.cat()')
+            return {}
+          }
+        }
+      }
+    },
     context({ req }) {
       // use the authenticate function from utils to auth req, its Async!
       return { user: null }
@@ -28,7 +78,7 @@ export const start = async () => {
   })
 
   await connect(config.dbUrl)
-  const { url } = await server.listen({ port: config.port })
+  const { url } = await server.listen({ port: 4500 })
 
   console.log(`GQL server ready at ${url}`)
 }
